b499416d2868247a781061c3ac8d167305ed236f
Bryan Ho <bryan.ho@utexas.edu>
2015-10-16 23:55:51 -0500

    new changes

--- 03 Visualizations/combineData.R
+++ 03 Visualizations/combineData.R
@@ -1,3 +1,2 @@
-baltimore %>% select(CURRENT_ANNUAL_SALARY)
 df2 <- data.frame(dplyr::bind_cols(florida, sample_n(baltimore, 9100)))
 df2 %>% select(DEPARTMENT, GENDER) %>% filter(DEPARTMENT %in% c("POL", "HHS")) %>% ggplot(aes(x = DEPARTMENT)) + geom_bar() + facet_grid(.~GENDER)+ labs(x="Department", y= "Count") + ggtitle("Comparison DEPARTMENTS VS. GENDERS")
